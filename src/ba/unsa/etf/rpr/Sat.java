20b6e0f8e8628b902ee31e357b12cd0f80284062
unknown <dbasalic1@etf.unsa.ba>
2019-10-12 19:50:12 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    private int sati, minute, sekunde;
+
+    public Sat(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+}
